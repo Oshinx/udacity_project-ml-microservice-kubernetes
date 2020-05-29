61eb94e5976f3d10cad006d550307b18124820b0
Ubuntu <ubuntu@ip-172-31-37-199.ec2.internal>
2020-05-29 16:11:41 +0000

    kubernetes-bash-finshed

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -3,16 +3,20 @@
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+# This is your Docker ID
+dockerpath=mikoski890/udacitylearnmirco
 
 # Step 2
-# Run the Docker Hub container with kubernetes
+# Run a Docker Hub container with kubernetes
+kubectl run udacity-project-ml-microservice-kubernetes-pod\
+    --image=$dockerpath\
+    --port=80 --labels app=udacity-project-ml-microservice-kubernetes-pod
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
-# Forward the container port to a host
-
+# Forward the container port to host
+kubectl port-forward ml-udacitylearnmirco-pod 80:8080
