70a231e13c35dccba1d9e0c4aaa119f58d159141
Ubuntu <ubuntu@ip-172-31-42-142.ec2.internal>
2020-05-30 23:36:06 +0000

    kubernetes bash  script configured

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -6,17 +6,16 @@
 # This is your Docker ID
 dockerpath=mikoski890/udacitylearnmirco
 
+
 # Step 2
-# Run a Docker Hub container with kubernetes
-kubectl run udacity-project-ml-microservice-kubernetes-pod\
-    --image=$dockerpath\
-    --port=80 --labels app=udacity-project-ml-microservice-kubernetes-pod
+# Run the Docker Hub container with kubernetes
+kubectl create deployment udacitydeploycluster08 --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 
-# Step 4:
-# Forward the container port to host
-kubectl port-forward ml-udacitylearnmirco-pod 80:8080
+# Step 
+# Forward the container port to a host
+kubectl create service nodeport   udacitydeploycluster08 --tcp=80:80
